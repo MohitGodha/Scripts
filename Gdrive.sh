05122bead4f8661f3ccdd7693c84b2b3a3cbf57b
Hidden <Hidden>
2016-02-21 18:04:16 +0530

    Grive Script

---
+++ Gdrive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#first Update
+sudo apt-get update
+#1st Command
+sudo add-apt-repository ppa:nilarimogard/webupd8
+#2nd Command
+sudo apt-get update
+#3rd Command
+sudo apt-get install grive-tools
